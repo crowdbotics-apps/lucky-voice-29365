e8cd6cdb7f5430b1c061acafca5e7833c2195a9f
Crowdbotics <team@crowdbotics.com>
2021-08-03 02:06:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lucky_voice_29365Identifier",
+  url: "https://lucky_voice_29365Identifier.botics.co",
+  api: "https://lucky_voice_29365Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
